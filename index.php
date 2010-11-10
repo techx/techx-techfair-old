6a8ac86991751fd99c7cbc5808f83cb74a534efd
Joshua Ma <me@joshma.com>
2010-11-09 23:24:21 -0500

    templating system uses routing.php for definition of pages, works with subfolders

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html> 
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
 require('parts/routing.php');
 require('parts/header.php');
 require('parts/nav.php');
 require('parts/footer.php');
 ?>
+<!DOCTYPE html>
 <html> 
 <head>
-<?php header($d['title']); ?>
+<?php make_header($d['title']); ?>
 </head>
 <body>
 <?php
-require($d['file']);
+require('pages/'.$d['content']);
 ?>
-</body
+</body>
 </html>
--- parts/routing.php
+++ parts/routing.php
@@ -8,12 +8,32 @@ $routes = array(
 		'title' => 'MIT TechFair / 404 Not Found',
 		'content' => '404.php'
 		),
+	'events' => array(
+		'title' => 'MIT TechFair / Events',
+		'content' => 'events/fair.php'
+		),
+	'events/banquet' => array(
+		'title' => 'MIT TechFair / Banquet',
+		'content' => 'events/banquet.php'
+		),
 );
-if (! isset($_GET['page'])) $d = $routes['home'];
-else if (isset($page[$_GET['page']])) $d = $routes[$_GET['page']];
+if (! isset($_GET['page']) || $_GET['page'] == '')
+{
+	$d = $routes['home'];
+}
 else
 {
-	header("HTTP/1.0 404 Not Found");
-	$d = $routes['404'];
+	$page = $_GET['page'];
+	if (substr($page,0,1) == '/') $page = substr($page,1);
+	if (substr($page,strlen($page)-1) == '/') $page = substr($page,0,strlen($page)-1);
+	if (isset($routes[$page]))
+	{
+		$d = $routes[$page];
+	}
+	else
+	{
+		header("Status: 404 Not Found");
+		$d = $routes['404'];
+	}
 }
 ?>
