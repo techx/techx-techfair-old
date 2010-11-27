312ae42654763d9bfa2dfbb227d89f588b0d3e49
Joshua Ma <me@joshma.com>
2010-11-26 23:33:51 -0500

    debugging file upload

--- pages/resume_action.php
+++ pages/resume_action.php
@@ -85,7 +85,7 @@ if(count($errors) == 0)
 
 		$dir = 'uploads/';
 		$filepath = $dir.$filename;
-
+		echo $filepath;
 		if (is_uploaded_file($_FILES['resume']['tmp_name']) && move_uploaded_file($_FILES['resume']['tmp_name'],$filepath))
 		{
 			$update = mysql_query("UPDATE resumedrop11 SET resume=$filepath WHERE id=$id");
