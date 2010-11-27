fc4c2f0d3a16e6de8d04eb11b094ada5e1666579
Joshua Ma <me@joshma.com>
2010-11-27 16:51:58 -0500

    past sponsors

---
+++ pages/about/pastsponsors.php
@@ -0,0 +1,112 @@
+<h1>Past Sponsors of TechFair<h1>
+	
+<h2>2010 MIT TechFair</h2>
+<p>2010 included the addition of some very big names in the technology industry, and the continuation of many of our loyal partners.</p>
+
+<h3>Platinum Sponsor</h3>
+<p>Oracle</p>
+<h3>Gold Sponsors</h3>
+<p>Microsoft, Thompson Reuters</p>
+<h3>Silver Sponsors</h3>
+<p>Lockheed Martin, Sandia National Laboratories, TomTom International B.V., Cisco
+Systems, Inc.</p>
+<h3>Basic Sponsors</h3>
+<p>Draper Laboratory, Medtronic, Cooliris, LogMeIn Inc., Tagged Inc, Activision, Akamai, Dropbox, Google, US Army Natick, Ball Aerospace &amp; Technologies Corp., Recombinant Data Corp., Global Prior Art, Grassroots Giving Group, RealNetworks, DRW Trading Group, Knight Equity Markets, L.P., General Atomics., Northrop Grumman, Autotegrity, GE Aviation, Human Dynamics Research Group, MIT Lincoln Laboratory, PROTEUS Technologies</p>
+
+<h2>2009 MIT TechFair</h2>
+<p>2009 continued Techfair's success with a strong group of gold and silver sponsors.</p>
+<h3>Gold Sponsors</h3>
+<p>BAE Systems, Microsoft, The Mathworks, Thomson Reuters, Bloomberg</p>
+<h3>Silver Sponsors</h3>
+<p>Opnet Technologies, Logmein, Maxim, Lockheed Martin, Schlumberger, DRW Trading
+Group, IGT, Cisco</p>
+<h3>Basic Sponsors</h3>
+<p>454 Life Sciences, Analog Devices, Aurora Flight Sciences, Shell, Cooliris, Glance Networks, Medtronic, Redi2 Technologies, US Army, Wacom, Real Software Systems, Tibco Software Inc., Northrop Grumman, Vertica Systems, Akamai Technologies, Elastra Corporation, IBM, KD Secure, ClickDiagnostics, Linden Lab, Philips North America, Trimble Navigation Limited, Baker Hughes, Scanscout, Onlatte, PadMapper, Sponty, DropBox, Sun Microsystems, Astaro, GE aviation, Robert Bosch llc., Soapstone Networks, Tap n' Tap</p>
+
+<h2>2007 MIT TechFair</h2>
+<p>The 2007 event saw TechFair continue its explosive growth as fifty-six companies, eight academic labs, and one student group exhibited for more than 2200 students.</p>
+<h3>Platinum Sponsor</h3>
+<p>Oracle</p>
+<h3>Gold Sponsors</h3>
+<p>The Mathworks, Microsoft, Texas Instruments, BAE Systems, Motorola, Cisco, ASML, Seagate SyntaxError</p>
+<h3>Silver Sponsors</h3>
+<p>Google, Sensata Technologies, Bose, Draper Laboratory, Sun Microsystems, Nokia, Nortel, Pfizer, Aleri, Autodesk, Invinity Ward, KLA Tencor, GE MDS, Shell Oil, Thomson, Vertex Pharmaceuticals, Schlumberger</p>
+<h3>Basic Sponsors</h3>
+<p>Merck, ClearPoint Metrics, Hillcrest Labs, Medtronic, StreamBase, Integrated DNA Technologies, Amgen, Yahoo!, Analog Devices, 454 Life Sciences, Air Products Ltd., BioDefense, Boeing, Wilson TurboPower, Electro Standards Laboratories, GE Healthcare, IBM, Lucasfilm Ltd., Power Systems Mfg., Roland, Pulmatrix, Holosonics, Vanu</p>
+
+<h2>2006 MIT TechFair</h2>
+<p>Thirty-eight companies and more than 1400 students attended the second MIT TechFair.</p>
+<h3>Platinum Sponsor</h3>
+<p>The Mathworks</p>
+
+<h3>Gold Sponsors</h3>
+<p>iRobot, Microsoft, Intel, Schlumberger, Merck</p>
+
+<h3>Silver Sponsors</h3>
+<p>Apple, Bose, Industrial Light &amp; Magic, Nokia, Raytheon, Sun Microsystems, Johnson &amp; Johnson (Cordis), Autodesk, Air Liquide, Organon Pharmaceuticals</p>
+<h3>Basic Sponsors</h3>
+<p>Analog Devices, Medtronic, BBN Technologies, Broadcom, ClearPoint Metrics, Natick Soldier Systems Center, MIT Lincoln Laboratory, Amgen, Vision Robotics, Demiurge Studios, Creare, Electro Standards Laboratory, Grandstream Networks, Rallypoint, Wyeth, Integrated DNA Technologies, Invitrogen, Pfizer, Air Products</p>
+
+<table align="center">
+    <tr>
+        <td>
+            <a href="/img/history/06_1.jpg" target="_blank"><img src="/img/history/06_1t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/06_2.jpg" target="_blank"><img src="/img/history/06_2t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/06_3.jpg" target="_blank"><img src="/img/history/06_3t.jpg" /></a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a href="/img/history/06_4.jpg" target="_blank"><img src="/img/history/06_4t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/06_5.jpg" target="_blank"><img src="/img/history/06_5t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/06_6.jpg" target="_blank"><img src="/img/history/06_6t.jpg" /></a>
+        </td>
+    </tr>
+</table>
+
+<h2>2005 MIT Tech Fair</h2>
+<p>On January 31st, 2005, over 700 students went to Walker Memorial on the MIT campus to attend MIT's first ever Tech Fair. Ten industry-leading companies presented in a trade show format some of their most innovative science and technologies. Everything from technologies protecting the soldier of the future to automating carpet vacuuming was displayed. See pictures of the 2005 exhibition.</p>
+<p>The success of the first ever Technology Fair is only the start of hopefully
+many more fairs to follow.</p>
+<p>The following companies attended the 2005 MIT Tech Fair:</p>
+<h3>Platinum Sponsor</h3>
+<p>VMware</p>
+<h3>Gold Sponsors</h3>
+<p>iRobot, IBM</p>
+<h3>Silver Sponsors</h3>
+<p>Bose, Payload Systems Inc., Raytheon</p>
+<h3>Other Companies</h3>
+<p>Natick Soldier Center, ArgonST, Synopsys, Navsys</p>
+
+<table align="center">
+    <tr>
+        <td>
+            <a href="/img/history/05_1.jpg" target="_blank"><img src="/img/history/05_1t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/05_2.jpg" target="_blank"><img src="/img/history/05_2t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/05_3.jpg" target="_blank"><img src="/img/history/05_3t.jpg" /></a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a href="/img/history/05_4.jpg" target="_blank"><img src="/img/history/05_4t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/05_5.jpg" target="_blank"><img src="/img/history/05_5t.jpg" /></a>
+        </td>
+        <td>
+            <a href="/img/history/05_6.jpg" target="_blank"><img src="/img/history/05_6t.jpg" /></a>
+        </td>
+    </tr>
+</table>
