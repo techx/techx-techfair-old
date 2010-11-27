df1ae52ec4692a3f31ad965cba5e75e7b868b1c0
Joshua Ma <me@joshma.com>
2010-11-27 16:24:49 -0500

    oops, added the moved pages

---
+++ pages/about/team.php
@@ -0,0 +1,79 @@
+<script type="text/javascript"> 
+var revert = new Array();
+var inames = new Array('Emily', 'Jeff', 'Kuan', 'Susie', 'Cyril', 'Nick');
+ 
+// Preload
+if (document.images) {
+  var flipped = new Array();
+  for(i=0; i< inames.length; i++) {
+    flipped[i] = new Image();
+    flipped[i].src = "/img/exec/"+inames[i]+"1.jpg";
+  }
+}
+ 
+function over(num) {
+  if(document.images) {
+    revert[num] = document.images[inames[num]].src;
+    document.images[inames[num]].src = flipped[num].src;
+  }
+}
+function out(num) {
+  if(document.images) document.images[inames[num]].src = revert[num];
+}
+</script>
+<h1>The TechFair Executive Team</h1>
+<p><strong>6 people, 1 awesome team.</strong></p>
+
+<h2>Emily Zhao</h2>
+<img src="/img/exec/Emily0.jpg" name="Emily" onMouseOver="over(0)" onMouseOut="out(0)" class="teamImage"/>
+<div class="team">
+	<h3>Managing Director, Logistics Director</h3>
+	<h4>Course 6+18</h4>
+	<span>The alpha female of the pack, Emily Zhao's only documented weakness is being distracted by an enticing Facebook app. In her off hours, Emily enjoys managing her other 30,000 student groups.  She can warp time and space to her will.</span>
+</div>
+<div class="clear"></div>
+
+<h2>Jeff Chen</h2>
+<img src="/img/exec/Jeff0.jpg" name="Jeff" onMouseOver="over(1)" onMouseOut="out(1)" class="teamImage"/>
+<div class="team">
+	<h3>Associate Director, Corporate Relations Director, Human Relations Director</h3>
+	<h4>Course 6+18</h4>
+	<span>When not frantically dashing from Techfair meeting to Techfair meeting, Jeff enjoys taking Jiujitsu classes and skateboarding. Jeff likes to think he looks cool skateboarding to class in uniform, when in fact he just resembles a teenage ghost.</span>
+</div>
+<div class="clear"></div>
+
+<h2>Kuan Cheng</h2>
+<img src="/img/exec/Kuan0.jpg" name="Kuan" onMouseOver="over(2)" onMouseOut="out(2)" class="teamImage"/>
+<div class="team">
+	<h3>Corporate Relations Director</h3>
+	<h4>Course 2</h4>
+	<span>Kuan directs the Corporate Relations board with a mixture of motherly love and a Stalin-esque iron fist. Last summer, she interned at General Electric. No she did not meet Jack Donaghey. Her favorite hobby is talking.</span>
+</div>
+<div class="clear"></div>
+
+<h2>Susie Fu</h2>
+<img src="/img/exec/Susie0.jpg" name="Susie" onMouseOver="over(3)" onMouseOut="out(3)" class="teamImage"/>
+<div class="team">
+	<h3>Marketing Director, IT Director</h3>
+	<h4>Course 6</h4>
+	<span>A woman of many talents, Susie can instantly identify the pitch of a fire alarm, or cook a vegetarian meal worthy of a brontosaurus (She doesn't care that they don't exist). In her leisure time she runs a small startup, Susie's Veggie Diner. It is often on fire.</span>
+</div>
+<div class="clear"></div>
+
+<h2>Cyril Lan</h2>
+<img src="/img/exec/Cyril0.jpg" name="Cyril" onMouseOver="over(4)" onMouseOut="out(4)" class="teamImage"/>
+<div class="team">
+	<h3>Finance Director</h3>
+	<h4>Course 6+15</h4>
+	<span>Cyril, being the oldest in the group, naturally is in charge of the money, which he doles out in small envelopes labeled 'allowance'. Outside of Techfair, Cyril sings for the MIT acapella group Syncopasian. Fun fact: The name 'Syncopasian' comes from the fact that all of its members belong to the Navajo tribe.</span>
+</div>
+<div class="clear"></div>
+
+<h2>Nick Dou</h2>
+<img src="/img/exec/Nick0.jpg" name="Nick" onMouseOver="over(5)" onMouseOut="out(5)" class="teamImage"/>
+<div class="team">
+	<h3>THINK Director</h3>
+	<h4>Course 2+6</h4>
+	<span>Nick is the head honcho of the THINK operation, which is kind of like babysitting, if your kids are all geniuses. When not taking far more classes than humanly possible, Nick regularly upholds the astonishing feat of owning a fixed-gear bicycle without being smug about it.</span>
+</div>
+<div class="clear"></div>
