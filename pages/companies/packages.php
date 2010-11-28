83f2a43d8c40367fd2ba05c68c5555507fb41dec
Joshua Ma <me@joshma.com>
2010-11-27 23:00:50 -0500

    added new files for students section

---
+++ pages/companies/packages.php
@@ -0,0 +1,111 @@
+<h1>Sponsorship Packages</h1>
+<p>TechFair offers three standard sponsorship packages, designated below. Aside from the amenities described, the higher sponsorships help companies maintain a larger physical and advertised presence as part of our TechFair campaign before and during the fair.</p>
+<h2>Packages</h2>
+<table id="sponsorship"> 
+	<tr> 
+		<th>Sponsorship</th> 
+		<td class="gold">Gold</td> 
+		<td class="silver">Silver</td> 
+		<td class="bronze">Bronze</td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Booths</th> 
+		<td>2 booths</td> 
+		<td>1 booth</td> 
+		<td>1 booth</td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Lodging</th> 
+		<td>2 hotel rooms</td> 
+		<td>1 hotel room</td> 
+		<td></td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Catering</th> 
+		<td>Breakfast and lunch</td> 
+		<td>Breakfast and lunch</td> 
+		<td>Breakfast and lunch</td> 
+	</tr> 
+ 
+	<tr> 
+		<th>TechTalk Options</th> 
+		<td>Spotlight</td> 
+		<td>Standard</td> 
+		<td></td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Storage &amp; Shipping</th> 
+		<td>Complimentary</td> 
+		<td>Optional extra</td> 
+		<td></td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Parking</th> 
+		<td>Complimentary</td> 
+		<td>Optional extra</td> 
+		<td></td> 
+	</tr> 
+ 
+	<tr> 
+		<th>A/V Support</th> 
+		<td>Complimentary</td> 
+		<td>Optional extra</td> 
+		<td></td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Banquet</th> 
+		<td>1 table</td> 
+		<td>1 table</td> 
+		<td>1 table</td> 
+	</tr> 
+ 
+	<tr> 
+		<th>Advertising</th> 
+		<td>Large logo/section in TechFair booklet</td> 
+		<td>Medium logo/section in TechFair booklet</td> 
+		<td>Small logo/section in TechFair booklet</td> 
+	</tr> 
+ 
+	<tr> 
+		<th></th> 
+		<td>Feature company videos on TechFair website</td> 
+		<td>Feature company videos on TechFair website</td> 
+		<td></td> 
+	</tr> 
+ 
+	<tr> 
+		<th></th> 
+		<td>Access to TechFair resume book</td> 
+		<td>Access to TechFair resume book</td> 
+		<td>Access to TechFair resume book</td> 
+	</tr> 
+ 
+	<tr> 
+		<th></th> 
+		<td>Free advertisement in The Tech, MIT's premier newspaper</td> 
+		<td></td> 
+		<td></td> 
+	</tr> 
+	
+	<tr> 
+		<th></th> 
+		<td>Customized TechFair marketing team campus advertising campaign</td> 
+		<td></td> 
+		<td></td> 
+	</tr> 
+	
+	<tr class="pricing"> 
+		<th>Price</th> 
+		<td>$4000</td> 
+		<td>$2000</td> 
+		<td>$1000</td> 
+	</tr> 
+</table>
+<h2>Apply</h2>
+<p>TechFair also offers custom sponsorships and opportunities. If interested in applying to MIT TechFair 2011, please contact <a href="mailto:techfair-cr-head@mit.edu">techfair-cr-head@mit.edu</a> for more information.</p> 
---
+++ pages/students/exhibit.php
@@ -0,0 +1,1 @@
+<h1>For Students</h1>
