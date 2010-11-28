47b09dd8022fc3e9245a041932f10f976fd2bc4a
Joshua Ma <me@joshma.com>
2010-11-28 14:50:42 -0500

    fixed into li, div.error

--- pages/students/exhibit.php
+++ pages/students/exhibit.php
@@ -2,18 +2,22 @@
 
 <h2>Our Vision for Technology</h2>
 <p>We at TechFair believe that innovative technology does not solely stem from a corporate or research setting. That is why, for the first time in TechFair history, we are opening up our booths to include individual student projects , student startups, and student groups on campus. Don't have a project? Come to the <a href="/events">fair</a> and support your friends!</p>
-<p>Learn more about how to get your own booth:<br>
--for your <a href="/mitstartups">student startup</a><br>
--for your <a href="/getabooth">individual project</a><br>
--for your student group, send us an <a href="mailto:">email</a>!<br>
+<p>Learn more about how to get your own booth:
+<ul>
+	<li>for your <a href="/mitstartups/">student startup</a></li>
+	<li>for your <a href="/getabooth/">individual project</a></li>
+	<li>for your student group, send us an <a href="mailto:">email</a>!</li>
+</ul>
 </p>
 
 
 <h2>TechFair Events</h2>
-<p>TechFair events are open to all MIT students: <br>
-- The <a href="/events/venue">Fair</a> itself is on spring registration day, January 31st, 2011. Come and check out awesome technology!<br>
-- Like to code? Check out the TechFair <a href="/hack">Hack-a-thon</a>, sponsored by Facebook.<br>
-- We will conclude TechFair day with a <a href="/events/banquet">networking banquet</a>.
+<p>TechFair events are open to all MIT students:
+<ul>
+	<li>The <a href="/events/venue/">Fair</a> itself is on spring registration day, January 31st, 2011. Come and check out awesome technology!</li>
+	<li>Like to code? Check out the TechFair <a href="/events/hackathon/">Hack-a-thon</a>, sponsored by Facebook.</li>
+	<li>We will conclude TechFair day with a <a href="/events/banquet/">networking banquet</a>.</li>
+</ul>
 </p>
 
 <h2>Resume Drop</h2>
