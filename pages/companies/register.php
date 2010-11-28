2a4a7ec8e672932cfd1e3260ea5347b74b6dd4a1
Joshua Ma <me@joshma.com>
2010-11-28 14:35:04 -0500

    register instead of apply, fb logo

---
+++ pages/companies/register.php
@@ -0,0 +1,2 @@
+<h1>2011 Corporate Registration</h1>
+<p>If you are an innovative company in the technology industry we'd love to have you at TechFair! To register for TechFair 2011, please email <a href="mailto:techfair-cr-head@mit.edu">techfair-cr-head@mit.edu</a>.</p>
