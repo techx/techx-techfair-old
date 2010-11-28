9352625aa63c19a2231e5409cdeb60dcae1d0da4
Joshua Ma <me@joshma.com>
2010-11-28 11:55:19 -0500

    touched up individual/student pages

--- pages/students/startups.php
+++ pages/students/startups.php
@@ -1,24 +1,17 @@
 <h1>Student Startups</h1>
-<p>
-Show off your startup to other students and companies the annual MIT TechFair technology expo! We are looking for technology oriented startups initiated by MIT students. The fair is on January 31st, 2011. 
-</p>
-<p>
-Why apply?
-<br>-You'll get a booth at TechFair, at no cost!
-<br>-Promote your startup to thousands of fellow students
-<br>-Make long lasting connections with companies
-<br>-Get inspired by other startups
-</p>
-<p>
-Note that startups must be majority composed of current MIT students. If you do not meet this criteria, we encourage you to learn more about coming to TechFair as <a href="/companies/">a company</a>.
-</p>
-<p>
-Preferred application (see below) deadline is <u>December 20 with rolling applications afterward</u>. Your startup must be prepared for interviews by the first week of January. 
-</p>
+<p>Show off your startup to other students and companies the annual MIT TechFair technology expo! We are looking for technology oriented startups initiated by MIT students. The fair is on January 31st, 2011.</p>
+<h2>Why apply?</h2>
+<ul>
+	<li>You'll get a booth at TechFair, at no cost!</li>
+	<li>Promote your startup to thousands of fellow students</li>
+	<li>Make long lasting connections with companies</li>
+	<li>Get inspired by other startups</li>
+</ul>
+<p>Note that startups must be mainly composed of current MIT students. If you do not meet this criteria, we encourage you to learn more about coming to TechFair as <a href="/companies/">a company</a>.</p>
+<p>Preferred application (see below) deadline is <u>December 20 with rolling applications afterward</u>. Your startup must be prepared for interviews by the first week of January.</p>
 
 <?php
-if ($_POST["name"] != "" and $_POST["email"] != "") {
-	echo '<h1>Thanks for your application, '.$_POST["name"].'! We will be in touch soon.</h1>';
+if ($_POST["name"] != "" and $_POST["email"] != ""):
 	$to = 'TechFair Task Force <techfair-tf@mit.edu>, '.$_POST["email"];
 	$subject = "TechFair Student Startup Application";
 	$message = 'Name: '.$_POST["name"].
@@ -29,16 +22,18 @@ if ($_POST["name"] != "" and $_POST["email"] != "") {
 	'<br><br>Startup name: '.$_POST["q2"].
 	'<br><br>Project demo: '.$_POST["q3"];
 	$headers = 'From: '.$_POST["name"].' <'.$_POST["email"].">\r\nContent-type: text/html\r\n".'X-Mailer: PHP/' . phpversion();
-	mail($to,$subject,$message,$headers);
-	$none = 'none';
-} else {
-	echo '<br><br><h1>Application</h1>';	
-}
+	if (mail($to,$subject,$message,$headers))
+	{
+		echo '<div class="success">Thanks for your application, ',$_POST["name"],'! We will be in touch soon.</div>';
+	} else {
+		echo '<div class="error">Our apologies, your application could not be submitted. Please send us an email at <a href="mailto:techfair-tf@mit.edu">techfair-tf@mit.edu</a>.</div>';
+	}
+else:
 ?>
 
-<div style="display: <?php echo $none?>">
+<h1>Application</h1>
+<form action="" method="POST">
 	<table> 
-		<form action="#" method="POST">
 		<tr> 
 			<th><label>Name</label></th> 
 			<td><input type="text" name="name" size="40" /></td> 
@@ -63,32 +58,36 @@ if ($_POST["name"] != "" and $_POST["email"] != "") {
 			<option value="5" >Grad</option>
 			</select>
 			</td> 
-		</tr> 
-		</table>
-		<table>
-		<tr> 
-			<th><label>What does your startup do? </label></th> 
 		</tr>
-		<tr>
-			<td><textarea name="q1" rows="5" cols="55"></textarea> </td> 
-		</tr> 
 		<tr> 
-			<th><label>What is your startup's name? </label></th> 
+			<th></th>
+			<td><label>What does your startup do? </label></td>
 		</tr>
-		<tr>
-			<td><textarea name="q2" rows="5" cols="55"></textarea> </td> 
+			<th></th>
+			<td><textarea name="q1" rows="5" cols="55"></textarea></td> 
 		</tr> 
 		<tr> 
-			<th><label>How will you demonstrate your project, and will it be demo-able by the end of IAP?</label></th> 
+			<th></th>
+			<td><label>What is your startup's name? </label></td> 
 		</tr>
 		<tr>
-			<td><textarea name="q3" rows="5" cols="55"></textarea> </td> 
+			<th></th>
+			<td><textarea name="q2" rows="5" cols="55"></textarea></td> 
+		</tr> 
+		<tr> 
+			<th></th>
+			<td><label>How will you demonstrate your project, and will it be demo-able by the end of IAP?</label></td> 
+		</tr>
+		<tr>
+			<th></th>
+			<td><textarea name="q3" rows="5" cols="55"></textarea></td> 
 		</tr> 
 		<tr>
+			<th></th>
 			<td>
-				<input type="submit" name="submit" value="Submit"/>
+				<button type="submit" name="submit">Submit</button>
 			</td>
 		</tr>
-		</form>	
-</table>
-</div>
+	</table>
+</form>
+<?php endif;?>
