cc0899bb69bb5433e97c90707c8dc03875f11f08
Joshua Ma <me@joshma.com>
2010-11-29 13:59:53 -0500

    redirect tf.mit.edu to techfair.mit.edu

--- parts/domain.php
+++ parts/domain.php
@@ -1,3 +1,7 @@
 <?php
-print_r($_SERVER);
+if ($_SERVER['HTTP_HOST']=='tf.mit.edu')
+{
+	header("HTTP/1.1 301 Moved Permanently"); 
+	header("Location: http://techfair.mit.edu".$_SERVER['REQUEST_URI']);
+}
 ?>
