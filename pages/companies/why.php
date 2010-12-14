747a9af48510c9ffd7e51a83cf2aaeda48ee71f0
Jennifer <jewang@mit.edu>
2010-12-13 23:34:29 -0500

    Fixed broken link on company 'why' page.

--- pages/companies/why.php
+++
@@ -1,9 +0,0 @@
-<h1>For Companies</h1>
-<h2>Exhibit at TechFair</h2>
-<p><strong>MIT TechFair</strong> is a one-day annual technology expo that aims to showcase the world's most innovative tech companies to MIT's best and brightest students. TechFair invites a broad spectrum of companies, from established multinationals to revolutionary new startups, to promote and demonstrate their products to an audience of several thousand MIT students pursuing careers in the industry.</p>
-<h2>Tech Talks</h2>
-<p>TechFair is proud to announce the addition of <strong>Tech Talks</strong> this year, a series of talks by distinguished speakers from our sponsor companies. Throughout the day, they'll be taking the center stage at TechFair to talk about their experiences in their industry, the technology their companies are developing, and - for MIT alumni - their journey since MIT. <strong>Gold sponsors</strong> are given the option for a spotlight Tech Talk of extended length, while <strong>silver sponsors</strong> have the opportunity for a standard Tech Talk.</p>
-<h2>Banquet</h2>
-<p>Another benefit of TechFair sponsorship is the post-fair banquet, a formal dinner where companies will be able to invite <strong>students of their choice</strong> to their reserved table. This is an excellent opportunity for sponsors to connect to MIT students at a different level.</p>
-<h2>Sponsorship Packages</h2>
-<p>For more information regarding how your company can be a part of TechFair, <a href="/exhibit/packages">read more about sponsorship packages</a>.</p>
