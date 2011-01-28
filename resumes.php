d89553cb1012114df7360210bac3913ad1a92e32
Joshua Ma <me@joshma.com>
2011-01-27 20:10:25 -0500

    resumes.php

--- resumes.php
+++ resumes.php
@@ -2,9 +2,86 @@
 <html>
 <head>
     <title>Resume Book</title>
+    <style>
+        body, html {
+            background-color: #222;
+            font-family: Arial, sans-serif;
+            color: #FFF;
+        }
+        h1 {
+            text-align: center;
+        }
+        table {
+            margin: 0 auto;
+            color: #000;
+            background-color: #FFF;
+        }
+        table td, table th {
+            padding: 5px 7px;
+        }
+        a {
+            color: #000;
+        }
+        a:hover {
+            color: #999;
+        }
+    </style>
 </head>
 <body>
     <h1>MIT TechFair 2011 Resume Book</h1>
+    <?php
+    function echoHeader($name,$key) {
+        if (!isset($_GET[$key])) echo '<a href="?',$key,'=1">',$name,'</a>';
+        elseif ($_GET[$key]==1) echo '<a href="?',$key,'=2">',$name,'</a>';
+        elseif ($_GET[$key]==2) echo '<a href="?">',$name,'</a>';
+    }
+    ?>
+    <table>
+        <tr>
+            <th><?php echoHeader('First Name','fname')?></th>
+            <th><?php echoHeader('Last Name','lname')?></th>
+            <th><?php echoHeader('Email','email')?></th>
+            <th><?php echoHeader('Year','year')?></th>
+            <th><?php echoHeader('Major(s)','major')?></th>
+            <th><?php echoHeader('Looking For','look')?></th>
+            <th>Resume</th>
+        </tr>
+        <tr>
+            <td>Joshua</td>
+            <td>Ma</td>
+            <td>joshma@mit.edu</td>
+            <td>2014</td>
+            <td>6-2</td>
+            <td>Internship</td>
+            <td><a href="#">Download</td>
+        </tr>
+        <tr>
+            <td>Joshua</td>
+            <td>Ma</td>
+            <td>joshma@mit.edu</td>
+            <td>2014</td>
+            <td>6-2</td>
+            <td>Internship</td>
+            <td><a href="#">Download</td>
+        </tr>
+        <tr>
+            <td>Joshua</td>
+            <td>Ma</td>
+            <td>joshma@mit.edu</td>
+            <td>2014</td>
+            <td>6-2</td>
+            <td>Internship</td>
+            <td><a href="#">Download</td>
+        </tr>
+        <tr>
+            <td>Joshua</td>
+            <td>Ma</td>
+            <td>joshma@mit.edu</td>
+            <td>2014</td>
+            <td>6-2</td>
+            <td>Internship</td>
+            <td><a href="#">Download</td>
+        </tr>
 <?php
 $mysqli = new mysqli('mysql.mit.edu','techfair','02139techfair','techfair+dayof');
 if (mysqli_connect_errno()) { 
@@ -18,5 +95,9 @@ while($row = $result->fetch_assoc()){
 $result->close();
 $mysqli->close();
 ?>
+<?php
+//endforeach;
+?>
+    </table>
 </body>
 </html>
