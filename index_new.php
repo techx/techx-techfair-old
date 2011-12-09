441c3ffda9fd60486df5f7d637c24fd0acad6c6f
Joshua Ma <me@joshma.com>
2011-12-08 21:12:11 -0500

    new index

--- index_new.php
+++ index_new.php
@@ -8,21 +8,90 @@
   var _gaq = _gaq || [];  _gaq.push(['_setAccount', 'UA-19969189-3']);  _gaq.push(['_trackPageview']);
   (function() {    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);  })();
 </script>
+    <style>
+        body {
+            background: #e6e6e6;
+            margin: 0;
+            padding: 0;
+        }
+        #container {
+            margin: 0 auto;
+            width: 1000px;
+            background: #FFF;
+        }
+        #header {
+            width: 100%;
+            height: 121px;
+            background: url(img/homepage/pixely.png) no-repeat top right;
+        }
+        #logo {
+            text-indent: -1000em;
+            width: 333px;
+            height: 74px;
+            margin: 10px 0 0 25px;
+            display: inline-block;
+            background: url(img/homepage/logo.png);
+        }
+        #nav {
+            width: 100%;
+            list-style: none;
+        }
+        #nav li{
+            display: block;
+            float: left;
+            
+        }
+        #slideshow {
+            clear: both;
+        }
+        #overlay {
+            width: 100%;
+            background: url();
+            
+        }
+        #linkout {
+            width: 100%;
+        }
+        #sponsors{
+            width: 100%;
+            height: 80px;
+            background: url(img/homepage/sponsorheader.png);
+        }
+    </style>
 
 </head>
 <body>
     <div id="container">
-        <div id="logo">
+        <div id="header">
+            <a id="logo">Techfair</a>
         </div>
-        <div id="nav">
-        </div>
+        <ul id="nav">
+            <li>
+                Events
+            </li>
+            <li>
+                For Students
+            </li>
+            <li>
+                For Companies
+            </li>
+            <li>
+                About Us
+            </li>
+        </ul>
         <div id="slideshow">
+        
         </div>
         <div id="overlay">
         </div>
-        <div id="linkout>
+        <div id="linkout">
+            <a href="mailto:techfair-exec@mit.edu">techfair-exec@mit.edu</a>
+            <a href="http://facebook.com/techfair"><img src="img/homepage/facebook.png" alt="fb" /></a>
+            <a href="http://twitter.com/mittechfair"><img src="img/homepage/twitter.png" alt="twitter" /></a>
+            <span id="copyright">&copy; MIT Techfair 2012</span>
         </div>
         <div id="sponsors">
+        
         </div>
     </div>
 </body>
