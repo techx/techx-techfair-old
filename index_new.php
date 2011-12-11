8137edc31e9d721e8c28188d703bc901a2ecf482
Joshua Ma <me@joshma.com>
2011-12-11 10:55:44 -0500

    linkout update

--- index_new.php
+++ index_new.php
@@ -51,15 +51,19 @@
         }
         #linkout {
             width: 100%;
+            color: #006cff;
+        }
+        #copyright {
+            float: right;
         }
         #sponsors{
             width: 100%;
             height: 80px;
             background: url(img/homepage/sponsorheader.png);
         }
-        
-       
-          /*make the navigation menu list*/
+
+
+        /*make the navigation menu list*/
         #nav {
             width: 1000px;
             list-style: none;
@@ -72,73 +76,73 @@
         /*apply general rules for menu items*/
         #nav li {
             float: left;
-			width: 250px;
-			text-align: center;
-			border-left:1px solid #666666;
-			box-sizing: border-box;
-			list-style:none;
-			background:#ffffff;
+            width: 250px;
+            text-align: center;
+            border-left:1px solid #666666;
+            box-sizing: border-box;
+            list-style:none;
+            background:#ffffff;
         }
         #nav li:first-child {
-        	border: 0;
+            border: 0;
         }
         #nav li:last-child {
-        	border: 0;
+            border: 0;
         }
         
         #nav > li > a{
-        	padding: 2px 0;
-        	display: block;
+            padding: 2px 0;
+            display: block;
         }
         
-		#subMenu {
-			height:0px;
-			width:1000px;
-			transition: height 0.3s;
-			-moz-transition: height 0.3s; /* Firefox 4 */
-			-webkit-transition: height 0.3s; /* Safari and Chrome */
-			-o-transition: height 0.3s; /* Opera */
-		}
-		#subMenu ul {
-        	list-style: none;
-        	margin: 0;
-        	padding: 0;
-        	width: 1000px;
+        #subMenu {
+            height:0px;
+            width:1000px;
+            transition: height 0.3s;
+            -moz-transition: height 0.3s; /* Firefox 4 */
+            -webkit-transition: height 0.3s; /* Safari and Chrome */
+            -o-transition: height 0.3s; /* Opera */
+        }
+        #subMenu ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            width: 1000px;
         }
       
-		#subMenu li {
-			border:0;
-			height:200px;
-			width:250px;
-			background:#ffffff;			
-		}
-		#nav a {
-			color: #000;
-			text-decoration: none;
-		}
-		#nav a:hover {
-			text-decoration: underline;
-		}
-		#subMenu a {
-			color: #ccc;
-			display:block;
-        	margin-top:15px;
+        #subMenu li {
+            border:0;
+            height:200px;
+            width:250px;
+            background:#ffffff;            
+        }
+        #nav a {
+            color: #000;
+            text-decoration: none;
+        }
+        #nav a:hover {
+            text-decoration: underline;
+        }
+        #subMenu a {
+            color: #ccc;
+            display:block;
+            margin-top:15px;
 
-			transition: color 0.3s;
-			-moz-transition: color 0.3s; /* Firefox 4 */
-			-webkit-transition: color 0.3s; /* Safari and Chrome */
-			-o-transition: color 0.3s; /* Opera */
-		}
-		/*Display the corresponding submenu on hover, keep it open while hovering over it */
-		#eMain:hover ~ #subMenu > ul > #eSub a, #eSub:hover a,
-		#sMain:hover ~ #subMenu > ul > #sSub a, #sSub:hover a,
-		#cMain:hover ~ #subMenu > ul > #cSub a, #cSub:hover a,
-		#aMain:hover ~ #subMenu > ul > #aSub a, #aSub:hover a {
-			color: #000;
-		}
-		#nav:hover #subMenu {
-			height:200px;
-		}
+            transition: color 0.3s;
+            -moz-transition: color 0.3s; /* Firefox 4 */
+            -webkit-transition: color 0.3s; /* Safari and Chrome */
+            -o-transition: color 0.3s; /* Opera */
+        }
+        /*Display the corresponding submenu on hover, keep it open while hovering over it */
+        #eMain:hover ~ #subMenu > ul > #eSub a, #eSub:hover a,
+        #sMain:hover ~ #subMenu > ul > #sSub a, #sSub:hover a,
+        #cMain:hover ~ #subMenu > ul > #cSub a, #cSub:hover a,
+        #aMain:hover ~ #subMenu > ul > #aSub a, #aSub:hover a {
+            color: #000;
+        }
+        #nav:hover #subMenu {
+            height:200px;
+        }
         
         
     </style>
@@ -150,27 +154,27 @@
             <a id="logo">Techfair</a>
         </div>
        <ul id="nav">
-			<li id="eMain">
-				<a href="#">Events</a>
-			</li>
-			<li id="sMain">
-				<a href="#">For Students</a>
-			</li>
-			<li id="cMain">
-				<a href="#">For Companies</a>
-			</li>
-			<li id="aMain">
-				<a href="#">About Us</a>
-			</li>
-			<li id="subMenu">
-				<ul>
-					<li id="eSub"><a href="#">Hack-a-thon</a> <a href="#">The Fair</a> <a href="#">TechTalks</a> <a href="#">Banquet</a> <a href="#">Afterparty</a></li>
-					<li id="sSub"><a href="#">2012 List of Exibitors</a> <a href="#">Startups & Projects</a> <a href="#">Project Funding</a> </li>
-					<li id="cSub"><a href="#">2012 List of Exibitors</a> <a href="#">Sponsorship Packages</a> <a href="#">Portal</a> </li>
-					<li id="aSub"><a href="#">Exec Team</a> <a href="#">Past Sponsors</a></li>
-				</ul>
-			</li>
-		</ul>
+            <li id="eMain">
+                <a href="#">Events</a>
+            </li>
+            <li id="sMain">
+                <a href="#">For Students</a>
+            </li>
+            <li id="cMain">
+                <a href="#">For Companies</a>
+            </li>
+            <li id="aMain">
+                <a href="#">About Us</a>
+            </li>
+            <li id="subMenu">
+                <ul>
+                    <li id="eSub"><a href="#">Hack-a-thon</a> <a href="#">The Fair</a> <a href="#">TechTalks</a> <a href="#">Banquet</a> <a href="#">Afterparty</a></li>
+                    <li id="sSub"><a href="#">2012 List of Exibitors</a> <a href="#">Startups & Projects</a> <a href="#">Project Funding</a> </li>
+                    <li id="cSub"><a href="#">2012 List of Exibitors</a> <a href="#">Sponsorship Packages</a> <a href="#">Portal</a> </li>
+                    <li id="aSub"><a href="#">Exec Team</a> <a href="#">Past Sponsors</a></li>
+                </ul>
+            </li>
+        </ul>
         <div id="slideshow">
         
         </div>
