adc4ceb3c449e996b2d289fdffa929b9e90e4d56
Joshua Ma <me@joshma.com>
2011-12-11 11:00:30 -0500

    font change

--- index_new.php
+++ index_new.php
@@ -3,16 +3,19 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>MIT Techfair</title>
+    <link href='http://fonts.googleapis.com/css?family=Galdeano' rel='stylesheet' type='text/css'>
     
-<script type="text/javascript">
-  var _gaq = _gaq || [];  _gaq.push(['_setAccount', 'UA-19969189-3']);  _gaq.push(['_trackPageview']);
-  (function() {    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);  })();
-</script>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];  _gaq.push(['_setAccount', 'UA-19969189-3']);  _gaq.push(['_trackPageview']);
+      (function() {    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);  })();
+    </script>
+    
     <style>
         body {
             background: #e6e6e6;
             margin: 0;
             padding: 0;
+            font-family: 'Galdeano', sans-serif;
         }
         #container {
             margin: 0 auto;
@@ -52,7 +55,7 @@
         #linkout, #linkout a {
             width: 100%;
             color: #006cff;
-            vertical-align: middle;
+            vertical-align: top;
         }
         #copyright {
             float: right;
