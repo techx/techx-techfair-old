6b71193f9cb629d9e852ba261583836f2627798e
Susie Fu <susiefu@mit.edu>
2012-01-13 15:40:39 -0500

    renamed inner page files

---
+++ pages/events/schedule.php
@@ -0,0 +1,19 @@
+<div id="column-top">
+	<h1>Events</h1>
+</div>
+<div id="column-left">
+	<h2>Hackathon Coding Competition</h2>
+	<p>Hackathon is happening the weekend before Techfair! This Facebook-sponsored event is an all-night coding competition. Learn more and sign up <a href="/hack">here</a>.
+	</p>
+	<h2>The Fair</h2>
+	<p>Techfair itself is on February 6th, 2012 in Rockwell Cage. <a href="/events/venue">Learn more</a> about the fair.
+	</p>
+</div>
+<div id="column-right">
+	<h2>The Banquet</h2>
+	<p>An annual <a href="/events/banquet">banquet</a> is held in celebration of the completion of <a href="/events/venue">Techfair</a> and to present the <a href="http://mittechfair.org/think/">THINK Competition</a> winners. MIT Students, check back in January on how you can sign up to attend.</p>
+	<h2>Afterparty</h2>
+	<p>For details of the Microsoft Afterparty, see the <a href="/events/afterparty/">Afterparty page</a>.</p>
+</div>
+<div id="column-bottom">
+</div>
