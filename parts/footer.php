a2250e6038eb4d3f6b51ef2892c5419a2a8f43cb
Tosh Goodson <goodsont@mit.edu>
2012-01-13 15:48:17 -0500

    Changed footer

--- parts/footer.php
+++ parts/footer.php
@@ -3,17 +3,22 @@ function make_footer()
 {
 ?>
 <div id="footer">
-	<div id="footer-content">
-		<div id="flickr"></div>
-		<div id="footer-links">
-			<ul>
-				<li><a href="/about/">about us</a></li>
-				<li><a href="/contact/">contact us</a></li>
-				<li id="copyright">copyright 2006-2011 MIT Techfair</li>
-			</ul>
-		</div>
-		<div style="clear:both"></div>
-	</div>
+	 <div id="linkout">
+            <a href="http://facebook.com/techfair"><img src="/img/homepage/facebook.png" alt="fb" /></a>
+            <a href="http://twitter.com/mittechfair"><img src="/img/homepage/twitter.png" alt="twitter" /></a>
+            <a href="mailto:techfair-exec@mit.edu">techfair-exec@mit.edu</a>
+            <span id="copyright">&copy; MIT Techfair 2012</span>
+        </div>
+        <div id="sponsors">
+            <h1>Attending Companies</h1>
+            <ul id="sponsorlinks">
+                <li id="facebook"><a href="http://www.facebook.com"></a></li>
+                <li id="adobe"><a href=http://www.adobe.com></a></li>
+                <li id="cisco"><a href=http://www.cisco.com></a></li>
+                <li id="dropbox"><a href=http://www.dropbox.com></a></li>
+                <li id="mozilla"><a href=http://www.mozilla.com></a></li>
+            </ul>  
+        </div>
 </div>
 <script type="text/javascript"> Cufon.now(); </script>
 <?php	
