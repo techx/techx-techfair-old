4e1c89224150477bd0baafabfc8f920c6fbb4ffe
Joshua Ma <me@joshma.com>
2012-01-13 17:36:41 -0500

    moving index

---
+++ index_old.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+//redirect tf.mit.edu to techfair.mit.edu
+require('parts/domain.php');
+//relationships between URLs and files
+require('parts/routing.php');
+//elements in <head>
+require('parts/head.php');
+//header of site, top banner and nav
+require('parts/header.php');
+//footer of site
+require('parts/footer.php');
+
+if(isset($_POST['action']) && isset($d['process']))
+{
+	require($d['process']);
+}
+
+//pick the right template
+if ($sectionName=='') {
+	require('parts/templates/home.php');
+} else if ($sectionName=='vvv') {
+    require('parts/templates/homeSAND.php');  
+} else {
+	require('parts/templates/default.php');
+}
+?>
