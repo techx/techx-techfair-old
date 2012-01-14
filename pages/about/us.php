b8b72eb4470861feaae75488835dd1d6e0e42807
Susie Fu <susiefu@mit.edu>
2012-01-14 17:20:20 -0500

    team pictures

--- pages/about/us.php
+++ pages/about/us.php
@@ -1,6 +1,12 @@
 <h1>About Us</h1>
-<p>Techfair is completely <strong>student run</strong>, led by passionate and dedicated students at the Massachusetts Institute of Technology. Members of the board are split into seven committees: Corporate Relations, Finance, Human Resources, Information Technology, Logistics, Marketing, and <a href="http://mittechfair.org/think/">THINK</a>.</p>
+<p>Techfair is completely <strong>student run</strong>, led by passionate and dedicated students at the Massachusetts Institute of Technology. Members of the board are split into eight committees: Corporate Relations, Marketing, Student Relations, Finance, Internal Relations, Information Technology, Logistics, and TechTalks.</p>
 <p>To contact us, email <a href="mailto:techfair-exec@mit.edu">techfair-exec@mit.edu</a>.</p>
-<h3>We are <span style="color:black">40 committee members</span> led by <span style="color:black">9 exec members</span> determined to throw<br /> <span style="color:black;font-size:30px">1 awesome event</span>.</h3>
+<p>Past Committees: <br>
+- <a href="http://mittechfair.org/think/">THINK</a>, a high school science and technology competition spun out of Techfair in 2008, now operates independently</p>
+
+<h3>We are <span style="color:black">45 committee members</span> led by <span style="color:black">10 exec members</span> determined to throw<br /> <span style="color:black;font-size:30px">1 awesome event</span>.</h3>
 <p>
-<img src="/img/about/group.jpg" alt="the Techfair team" />
+  <br><Br>
+<a href="/img/team/Team1o.jpg"><img src="/img/team/Team1.jpg" style="margin-right: 15px;" alt="The Techfair team" /></a>
+<a href="/img/team/Team2o.jpg"><img src="/img/team/Team2.jpg" style="margin-right: 15px;"  alt="At our annual retreat" /></a>
+<a href="/img/team/Team3o.jpg"><img src="/img/team/Team3.jpg" alt="We have fun, don't worry" /></a>
