36df05ba1fca2ae4c26e263434184e33e52381c8
Tosh Goodson <goodsont@mit.edu>
2012-01-14 13:42:46 -0500

    Copied the funding page from the website

---
+++ pages/students/funding.php
@@ -0,0 +1,25 @@
+<h2>Techfair Project Funding</h2>
+<p>Got an idea for a really cool project to build over Fall 2011? Techfair is offering $500 per  project of funding for those that need an extra boost to get off the ground. Projects are funded in a two-stage check-in and reimbursement process during Fall 2011. 
+<h3>Application &amp; Funding Dates</h3>
+<ul>
+<li>9/25 - Applications Due</li>
+<li>9/29 - Responses out to schedule interviews</li>
+<li>10/1-10/6 - Interviews</li>
+<li>10/14-10/17 - Acceptances out</li>
+<li>10/17 - Deadline to decline funding</li>
+<li>11/28-12/3 - Check-in, reimbursement #1</li>
+<li>mid-December/early IAP - Check-in, reimbursement #2</li>
+</ul>
+<h3>F.A.Q.</h3>
+<ul>
+<li><strong>Can I apply for multiple projects?</strong><br />Yes! However, you are likely to only receive funding for one project.</li>
+<li><strong>Can I apply for startup funding?</strong><br />You may apply for funding any new technologies or products your startup is developing.</li>
+<li><strong>Can I apply for funding for an ongoing project?</strong><br />Yes, but please provide documentation on previous work completed and any prior funding.<br /></li>
+<li><strong>What should I bring to the interview and what are you looking for?</strong><br />
+We are looking for projects with a solid plan and people who are determined to complete their project during Fall 2011/IAP 2012. At your interview, we will want you to pitch your project in person and demonstrate that you have considered many of the project's details. Of course, we understand that projects can take unexpected turns. You will have an opportunity to update your budget and revise your plans at check-in #1. Bring any technical plans, documentation, and evidence supporting your project. Interest in displaying your project at Techfair 2012 is also a plus.</li>
+</ul>
+<p>Any other questions? Drop us an email at <a href="techfair-sr@mit.edu">techfair-sr@mit.edu</a> with Techfair Project Funding Question in the subject line.</p>
+<h3>Application Form</h3>
+<p>
+Our application is closed for Fall 2011. 
+</p>
