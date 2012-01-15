c4181a066829bbdf80db77d040dcfd0bbfb016dc
Tosh Goodson <goodsont@mit.edu>
2012-01-15 15:03:15 -0500

    Fixed THINK line

--- pages/about/us.php
+++ pages/about/us.php
@@ -1,8 +1,6 @@
 <h1>About Us</h1>
-<p>Techfair is completely <strong>student run</strong>, led by passionate and dedicated students at the Massachusetts Institute of Technology. Members of the board are split into eight committees: Corporate Relations, Marketing, Student Relations, Finance, Internal Relations, Information Technology, Logistics, and TechTalks.</p>
+<p>Techfair is completely <strong>student run</strong>, led by passionate and dedicated students at the Massachusetts Institute of Technology. Members of the board are split into eight committees: Corporate Relations, Marketing, Student Relations, Finance, Internal Relations, Information Technology, Logistics, and TechTalks. We are also affiliated with <a href="http://mittechfair.org/think/">THINK</a>, a high school science and technology competition spun out of Techfair in 2008.</p>
 <p>To contact us, email <a href="mailto:techfair-exec@mit.edu">techfair-exec@mit.edu</a>.</p>
-<p>Past Committees: <br>
-- <a href="http://mittechfair.org/think/">THINK</a>, a high school science and technology competition spun out of Techfair in 2008, now operates independently</p>
 
 <h3>We are <span style="color:black">45 committee members</span> led by <span style="color:black">10 exec members</span> determined to throw<br /> <span style="color:black;font-size:30px">1 awesome series of events</span>.</h3>
 <p>
