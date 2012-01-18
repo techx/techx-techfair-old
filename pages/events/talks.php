0c55b965dce22794f78597b14c18be149cd425b6
Tosh Goodson <goodsont@mit.edu>
2012-01-18 17:42:17 -0500

    changed order of info on techtalks

--- pages/events/talks.php
+++ pages/events/talks.php
@@ -1,9 +1,9 @@
 <h1>Techtalks</h1>
 <h2>About</h2>
 <p>Introducing the newest Techfair event! Come hear industry leaders and visionaries talk about their own perspectives on various aspects of tech and innovation.</p>
-<h2>Location</h2>
-<p>Room 10-250</p>
-<h2>Time</h2>
-<p>February 11 (the Saturday after Techfair) - exact times to be announced</a>
 <h2>Attendees</h2>
 <p>Everyone is invited - Boston area students, entrepreneurs, and tech enthusiasts alike.</p>
+<h2>Time</h2>
+<p>February 11 (the Saturday after Techfair) - exact times to be announced</a>
+<h2>Location</h2>
+<p>Room 10-250</p>
