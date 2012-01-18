1db00d3fb65c38dc3cd8fb27c0f4a1e59c1a9a8f
Joshua Ma <me@joshma.com>
2012-01-18 18:31:54 -0500

    analytics

--- parts/analytics.php
+++ parts/analytics.php
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-19969189-3']);
+  _gaq.push(['_setAccount', 'UA-28455215-1']);
   _gaq.push(['_trackPageview']);
 
   (function() {
