b2cf5c04b72eae94fba46653497c8a2f15814f45
Susie Fu <susiefu@mit.edu>
2012-01-30 19:02:18 -0500

    adding techtalk time

--- pages/events/talks.php
+++ pages/events/talks.php
@@ -4,6 +4,9 @@
 <h2>Attendees</h2>
 <p>Everyone is invited - Boston area students, entrepreneurs, and tech enthusiasts alike.</p>
 <h2>Time</h2>
-<p>February 11 (the Saturday after Techfair) - exact times to be announced</a>
+<p>February 11, 2012 (the Saturday after Techfair)
+  <br>11AM - 5PM
+  <br>Lunch will be provided
+  </p>
 <h2>Location</h2>
 <p>Room 10-250</p>
