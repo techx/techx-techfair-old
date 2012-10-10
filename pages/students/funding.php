d6c71ba290188f64d087a0d786b13cdcec128341
Ben Frank <benjaminsfrank@gmail.com>
2012-10-10 05:42:23 -0300

    changed project funding deadlines, info sessions

--- pages/students/funding.php
+++ pages/students/funding.php
@@ -2,14 +2,22 @@
 <p>Techfair is offering up to $500 per project of funding for those that need an extra boost to get off the ground. Projects are funded in a two-stage check-in and reimbursement process during the fall semester. 
 <h2>Application &amp; Funding Dates</h2>
 <p>
-  For Fall 2011 funding cycle:
+  For Fall 2012 funding cycle:
 <ul>
-<li>10/15 - Applications Due</li>
-<li>10/17 - Responses out to schedule interviews</li>
-<li>10/17-20 - Interviews</li>
-<li>10/23 - Acceptances out</li>
+<li>10/20 - Applications Due (Saturday at 11:59pm)</li>
+<li>10/23 - Responses out to schedule interviews</li>
+<li>10/25-27 - Interviews</li>
+<li>10/30 - Acceptances out</li>
 <li>11/26-30 - Check-in, reimbursement #1</li>
 <li>mid-December/early IAP - Check-in, reimbursement #2</li>
+</ul>
+</p>
+<h2>Info Sessions</h2>
+<p>We're holding two info sessions which will both be about an hour each.</p>
+<p>
+<ul>
+<li>Info session #1: 10/12 at 5pm (Friday)</li>
+<li>Info session #2: 10/18 at 9pm (Thursday)</li>
 </ul>
 </p>
 <h2>F.A.Q.</h2>
@@ -19,7 +27,7 @@
 <li><strong>Can I apply for startup funding?</strong><br />You may apply for funding any new technologies or products your startup is developing.</li>
 <li><strong>Can I apply for funding for an ongoing project?</strong><br />Yes, but please provide documentation on previous work completed and any prior funding.<br /></li>
 <li><strong>What should I bring to the interview and what are you looking for?</strong><br />
-We are looking for projects with a solid plan and people who are determined to complete their project during Fall 2011/IAP 2012. At your interview, we will want you to pitch your project in person and demonstrate that you have considered many of the project's details. Of course, we understand that projects can take unexpected turns. You will have an opportunity to update your budget and revise your plans at check-in #1. Bring any technical plans, documentation, and evidence supporting your project. Interest in displaying your project at Techfair 2012 is also a plus.</li>
+We are looking for projects with a solid plan and people who are determined to complete their project during Fall 2012/IAP 2013. At your interview, we will want you to pitch your project in person and demonstrate that you have considered many of the project's details. Of course, we understand that projects can take unexpected turns. You will have an opportunity to update your budget and revise your plans at check-in #1. Bring any technical plans, documentation, and evidence supporting your project. Interest in displaying your project at Techfair 2013 is also a plus.</li>
 </ul>
 </p>
 <p>Any other questions? Drop us an email at <a href="techfair-sr@mit.edu">techfair-sr@mit.edu</a> with Techfair Project Funding Question in the subject line.</p>
