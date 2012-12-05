79f1fba731ae13665af215f12a67922a84a808f4
Joshua Ma <me@joshma.com>
2012-12-04 23:09:34 -0500

    update intro page

--- pages/companies/intro.php
+++ pages/companies/intro.php
@@ -1,34 +1,69 @@
 <h1>Welcome to Techfair!</h1>
 <h2>About Techfair: what to expect</h2>
-<p><em>Techfair 2013 is scheduled for Feb 4, 2012, the day before classes start, from 10AM-3PM (setup starts at 7AM).</em></p>
+<p><em>Techfair 2013 is scheduled for <strong>Feb 4, 2013</strong>, the day
+before classes start, from <strong>10AM-3PM</strong> (setup starts at
+<strong>7AM</strong>).</em></p>
 
-<p>Techfair is not your average career fair. Now in its fifth year, Techfair is a new take on how recruiting should look like at MIT. At this Institute, you'll find some of the most passionate and curious minds out there, and you'll find that appealing to their intellectual interests can be one of the most effective ways of persuading a student to join your cause.</p>
+<p>Techfair is not your average career fair. Now in its fifth year, Techfair is
+a new take on how recruiting should look like at MIT. At this Institute, you'll
+find some of the most passionate and curious minds out there, and you'll find
+that appealing to their intellectual interests can be one of the most effective
+ways of persuading a student to join your cause.</p>
 
-<p>Our mission is to bring technology to the recruiting process. We believe that, by bringing tech demos, excitement, and fun to the average career fair we can create an experience that benefits all parties involved.</p>
+<p>Our mission is to bring technology to the recruiting process. We believe
+that, by bringing tech demos, excitement, and fun to the average career fair we
+can create an experience that benefits all parties involved.</p>
 
-<p>We're extremely excited to have you and your organization on board - the fair is not too far away, and we have a lot of work to do together!</p>
+<p>We're extremely excited to have you and your organization on board - the
+fair is not too far away, and we have a lot of work to do together!</p>
 
 <h2>Important Deadlines</h2>
 <ul>
   <li>Company registration is initiated by an official invoice, sent by your CR liaison.</li>
-  <li>1 week after receiving portal login information: company logo, name (as displayed on marketing materials), and description must be submitted to the logistics portal.</li>
-  <li>Jan 7: The rest of the logistics portal must be completely filled out. This includes crucial details like A/V requests, the representatives attending, hotel needs, and more. You'll receive more details on hotel and A/V within the next month, as soon as details are settled on our end.</li>
-  <li>Jan 21: All shipped packages should be received. We allot extra time so we can notify you of any missing packages and resolve these issues before the fair!</li>
-  <li>Jan 31: Payments due (see below for more information).</li>
+  <li><strong>1 week after receiving portal login information:</strong> company logo, name (as displayed on marketing materials), and description must be submitted to the logistics portal.</li>
+  <li><strong>Jan 7:</strong> The rest of the logistics portal must be completely filled out. This includes crucial details like A/V requests, the representatives attending, hotel needs, and more. You'll receive more details on hotel and A/V within the next month, as soon as details are settled on our end.</li>
+  <li><strong>Jan 21:</strong> All shipped packages should be received. We allot extra time so we can notify you of any missing packages and resolve these issues before the fair!</li>
+  <li><strong>Jan 31:</strong> Payments due (see below for more information).</li>
 </ul>
 
 <h2>Logistical Portal</h2>
-<p>In the coming weeks you will receive your login information for the logistics portal, an online website. This will be the primary means of collecting and aggregating company data.</p>
+<p>You can access the logistics portal at <a
+href="http://techfair.mit.edu/portal">http://techfair.mit.edu/portal</a>. The
+portal will be the primary means of collecting and aggregating company data.
+For logistical purposes, it is essential that all required details are filled
+out in time.</p>
+
+<p>Please add your company logo, display name, and description within one week of invoicing. 
+We’ll need this information for marketing materials and for the booths themselves, so it’s 
+important that we get it as early as possible.</p>
+
+<p>The remaining sections of the portal need to be filled out by January 7 so that we can secure 
+all parking spots, hotel rooms, etc in time. If you have any extenuating circumstances and 
+won’t be able to complete the portal in time, please let your CR liaison know.</p>
 
 <h2>Booths</h2>
-<p>Company booths are the core of Techfair - they're a reflection of both the company and the fair. Students are looking for interesting and engaging booths with technology demos they can learn about and ideally try out.</p>
+<p>Company booths are the core of Techfair - they're a reflection of both the
+company and the fair. Students are looking for interesting and engaging booths
+with technology demos they can learn about and ideally try out.</p>
 
-<p>We encourage every company to invest time into their Techfair display! We've seen incredible displays from a wide variety of companies. Organizations with tangible products have brought in prototypes of their latest models and designs, while software companies tend to demo their latest (and sometimes unreleased) products on large gorgeous displays. Others with products that are more difficult to demonstrate have instead compiled videos, infographics, and posters on the impact of their work and the science or engineering behind them. Just for fun, we've also seen baristas, popcorn machines, and swag for attendees.</p>
+<p>We encourage every company to invest time into their Techfair display! We've
+seen incredible displays from a wide variety of companies. Organizations with
+tangible products have brought in prototypes of their latest models and
+designs, while software companies tend to demo their latest (and sometimes
+unreleased) products on large gorgeous displays. Others with products that are
+more difficult to demonstrate have instead compiled videos, infographics, and
+posters on the impact of their work and the science or engineering behind them.
+Just for fun, we've also seen baristas, popcorn machines, and swag for
+attendees.</p>
 
-<p>If you have any ideas you want to run by us or any questions, don't hesitate to reach out.</p>
+<p>If you have any ideas you want to run by us or any questions, don't hesitate
+to reach out.</p>
 
 <h2>Invoice and Payments</h2>
-<p>You'll receive an invoice upon being confirmed for acceptance into the fair. Payment should be completed by January 31st. If you prefer to pay by credit card, let your CR liaison know and we can provide a link to pay online. Otherwise, please send all checks to:</p>
+<p>You'll receive an invoice upon being confirmed for acceptance into the fair.
+Payment should be completed by January 31st. If you prefer to pay by credit
+card, let your CR liaison know and we can provide a link to pay online.
+Otherwise, please send all checks to:</p>
 
 <p>
 <strong>MIT Technology Fair</strong><br />
@@ -38,7 +73,10 @@ Cambridge, MA 02139
 </p>
 
 <h2>Packages and Shipping</h2>
-<p>MIT Mail Services will be receiving and storing packages intended for use at Techfair. To ensure that you receive all of your shipments on the day of the fair, please send your packages to the exact address below (note that it is different from the invoice address):</p>
+<p>MIT Mail Services will be receiving and storing packages intended for use at
+Techfair. To ensure that you receive all of your shipments on the day of the
+fair, please send your packages to the exact address below (note that it is
+different from the invoice address):</p>
 
 <p>
 <strong>Techfair</strong><br />
@@ -47,16 +85,43 @@ WW15<br />
 Cambridge, MA 02139
 </p>
 
-<p>Packages mailed must be received at this address by January 21st. We will be taking inventory of the packages soon after this deadline and will notify you of how many of your packages we have received. Because MIT Mail Services receives and sorts a large number of packages a day, mislabeled items have a high chance of not being sorted into Techfair's group of packages. In order to prevent avoidable delays, please be sure your packages are labeled clearly with Techfair and your company's name.</p>
+<p>Packages mailed must be received at this address by January 21st. We will be
+taking inventory of the packages soon after this deadline and will notify you
+of how many of your packages we have received. Because MIT Mail Services
+receives and sorts a large number of packages a day, mislabeled items have a
+high chance of not being sorted into Techfair's group of packages. In order to
+prevent avoidable delays, please be sure your packages are labeled clearly with
+Techfair and your company's name.</p>
 
-<p>If you are shipping your packages back right after the fair, we will provide you FedEx labels and cover the cost of shipping. We will also include stickers to place on your packages that indicate your packages are to be shipped that day. Please attach both the label and sticker to your packages and leave your packages in front of your booth to be collected.</p>
+<p>If you are shipping your packages back right after the fair, we will provide
+you FedEx labels and cover the cost of shipping. We will also include stickers
+to place on your packages that indicate your packages are to be shipped that
+day. Please attach both the label and sticker to your packages and leave your
+packages in front of your booth to be collected.</p>
 
-<p>If you are storing your packages overnight for Tau Beta Pi's career fair, we will provide you stickers that indicate your package is to be stored overnight. Please attach the sticker to the front of your packages and leave your packages in front of your booth to be collected.</p>
+<p>If you are storing your packages overnight for Tau Beta Pi's career fair, we
+will provide you stickers that indicate your package is to be stored overnight.
+Please attach the sticker to the front of your packages and leave your packages
+in front of your booth to be collected.</p>
 
 <h2>A/V Services</h2>
-<p>Based on feedback and past experience, Techfair is simplifying its A/V services to instead offering a selection of a) PC Laptop, b) 32. LCD Monitor, and c) 40. LCD Monitor. Companies can choose any or all of the three, and Techfair will provide them at no extra cost. You can request these items through the logistics portal in the coming weeks.</p>
+<p>Based on feedback and past experience, Techfair is simplifying its A/V
+services to instead offering a selection of a) PC Laptop, b) 32” LCD Monitor,
+and c) 40” LCD Monitor. Companies can choose any or all of the three, and
+Techfair will provide them at no extra cost. You can request these items
+through the logistics portal.</p>
 
-<p><em>(These do cost us $150, $150, and $200 respectively to rent from MIT A/V services, however, so we kindly ask that companies only request items as necessary!)</em></p>
+<p><em>(These do cost us $150, $150, and $200 respectively to rent from MIT A/V
+services, however, so we kindly ask that companies only request items as
+necessary!)</em></p>
+
+<h2>Hotel, Banquet, and Further Updates</h2>
+<p>We will be contacting you in the weeks to come with updates, where
+applicable, regarding hotel, banquet, and other miscellaneous details. We will
+be updating the logistics portal to collect any additional information we may
+need from you.</p>
 
 <h2>Contact</h2>
-<p>Feel free to contact us with any questions or comments. Your corporate relations (CR) liaison is always available to answer any questions, or you can reach us at techfair-cr@mit.edu and techfair-exec@mit.edu.</p>
+<p>Feel free to contact us with any questions or comments. Your corporate
+relations (CR) liaison is always available to answer any questions, or you can
+reach us at techfair-cr@mit.edu and techfair-exec@mit.edu.</p>
