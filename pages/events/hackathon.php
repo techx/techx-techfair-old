cf61d0a3a7ac8de9d40114eaf06154caf9713f3c
Max Kolysh <maxko87@gmail.com>
2013-01-13 16:40:03 -0500

    2013 hackathon page update

--- pages/events/hackathon.php
+++ pages/events/hackathon.php
@@ -2,35 +2,54 @@
 <?php if($_GET['msg']=='success'):?>
 <div class="success">Your email has been saved. Thank you for your interest in MIT Techfair!</div>
 <?php endif;?>
-<h1>The&nbsp;<img src="/img/fb_logo.png" style="vertical-align:bottom" alt="facebook"/>&nbsp;Hackathon</h1>
+<h1>
+  <div class="column-left">
+    The&nbsp;<img height="35" width="140" src="/img/palantir_logo.png" style="vertical-align:-12%" alt="palantir"/>&nbsp;Hackathon
+  </div>
+  <div class="column-right">
+    <a href=[eventbrite_link]>Registration is open!</a>
+  </div>
+</h1>
 </div>
 
 <div class="column-left">
-<!--<h2>Interested?</h2>
-<!--
-<div id="facebook" class="interest">
-	<p>Enter your MIT email for more details:</p>
-	<?php if (isset($error['email'])) echo '<div class="error">',$error['email'],'</div>';?>
-	<form action="" method="post">
-		<input type="hidden" name="action" value="hackathon" />
-		<input type="text" name="email" />
-		<button type="submit">Submit</button>
-	</form>
-</div>
+  <!--<h2>Interested?</h2>
+  <!--
+  <div id="facebook" class="interest">
+  	<p>Enter your MIT email for more details:</p>
+  	<?php if (isset($error['email'])) echo '<div class="error">',$error['email'],'</div>';?>
+  	<form action="" method="post">
+  		<input type="hidden" name="action" value="hackathon" />
+  		<input type="text" name="email" />
+  		<button type="submit">Submit</button>
+  	</form>
+  </div>
 
-<p><a href="https://www.facebook.com/events/226472340766270/">Sign up</a> on the official Facebook event page.</p>
-<p>Please RSVP to this event and add your first and last name and school to the event wall.</p>-->
+  <p><a href="https://www.facebook.com/events/226472340766270/">Sign up</a> on the official Facebook event page.</p>
+  <p>Please RSVP to this event and add your first and last name and school to the event wall.</p>-->
 
-<h2>About</h2>
-<p>Calling all MIT, Harvard, and Brown engineering students! We are co-hosting with Facebook the first Battle of Boston Hackathon, an all-night coding competition.</p>
-<p>At most 4 people per team.</p>
-<p>Plenty of free food, caffeine, and swag! Prizes given every hour.</p>
+  <h2>About</h2>
+    <p>Techfair and Palantir are teaming up to bring you MIT’s biggest hackathon of the year. Represent your school and compete against college students of the northeast. </p>
+    
+  <h2>Schedule</h2>
+    <p>
+
+      <b>Saturday, 2/2 </b><br>
+      8:00pm: Doors open <br>
+      8:30pm: Dinner is served <br>
+      9:00pm: Hackathon Kickoff with [speaker] from Palantir <br>
+      <br>
+      <b>Sunday, 2/3 </b><br>
+      1:00pm: Hacking ends, lunch is served <br>
+      1:30pm: Presentations and awards <br>
+
+    </p>
 
 </div>
 
 <div class="column-right">
 	<h2>Attendees</h2>
-	<p>MIT, Harvard, and Brown students</p>
+	  <p>All university students are invited to compete.</p>
   <!--<h2>Prizes</h2>
   <p>Hackathon winners from each school will receive a pair of Beats by Dr. Dre headphones and a trip to Palo Alto to compete with winning teams from other university Hackathons</p>
 	<p>Second prize winners from each school will be rewarded with Diddy Beats</p>
@@ -39,6 +58,11 @@
   <p>Saturday, February 4 @ 8 PM to Sunday, February 5 @ 5 PM</p>
   <h2>Location</h2>
   <p>26-152</p>-->
+
+  <h2>Rookie Division</h2>
+    <p>If you don't have much experience coding, enter our <b>rookie</b> division. This is a great chance to expand your programming skills, 
+      meet new people, and win some awesome prizes as well!</p>
+
 </div>
 
 <div class="column-bottom" style="text-align: center; padding-top: 30px;">
