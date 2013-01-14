ed85db2c2ef545b34202502bf354606ad7235411
Joshua Ma <me@joshma.com>
2013-01-14 12:22:25 -0500

    snapshot frontpage work in progress

--- parts/templates/home.php
+++ parts/templates/home.php
@@ -6,6 +6,7 @@
   <title>MIT Techfair</title>
     
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,300,600' rel='stylesheet' type='text/css'>
+  <link href="css/typicons_kit/css/typicons.css" rel="stylesheet" />
   <link href="css/style.css" rel="stylesheet" />
   <link href="css/home.css" rel="stylesheet" />
 
@@ -19,24 +20,136 @@
 
       <div class="carousel">
         <div class="caption">
-          <h1>A new approach to career fair.</h1>
+          <h1>A new approach to career fairs.</h1>
           <p>Techfair is a student-run organization dedicated to bringing technology and recruiting to campus in a fresh, new way.</p>
+        </div>
+      </div>
+      <div class="for-companies">
+        Sponsorships are sold out for Techfair 2013. Are you a company attending Techfair 2013? Check out <a href="/companies/info/">our company info page</a>.
+      </div>
+
+      <div class="schedule">
+        <h3>2013 Schedule</h3>
+        <div class="event">
+          <span class="event-name">Hackathon</span>
+          <span class="time-item">Feb 2 <strong>8PM</strong></span>
+          <span class="desc-item">doors open</span>
+          <span class="desc-item"><a href="http://goo.gl/maps/R3EKa">Stata Center <span class="typicn export"></span></a></span>
+          <span class="time-item">Feb 3 <strong>1PM</strong></span>
+          <span class="desc-item">hacking ends</span>
+          <span class="time-item no-date"><strong>1:30PM</strong></span>
+          <span class="desc-item">presentation &amp; awards</span>
+          <div class="clearfix"></div>
+        </div>
+        <div class="event">
+          <span class="event-name">Fair</span>
+          <span class="time-item">Feb 4 <strong>10AM</strong></span>
+          <span class="desc-item">doors open</span>
+          <span class="time-item no-date"><strong>3PM</strong></span>
+          <span class="desc-item">fair concludes</span>
+          <div class="clearfix"></div>
+        </div>
+        <div class="event">
+          <span class="event-name">Banquet</span>
+          <span class="time-item no-date"><strong>6PM</strong></span>
+          <span class="desc-item">networking kickoff</span>
+          <span class="desc-item"><a href="http://goo.gl/maps/d6YFp">Media Lab<span class="typicn export"></span></a></span>
+          <span class="time-item no-date"><strong>8:30PM</strong></span>
+          <span class="desc-item">banquet concludes</span>
+          <div class="clearfix"></div>
+        </div>
+        <div class="event">
+          <span class="event-name">Afterparty</span>
+          <span class="time-item no-date"><strong>9PM</strong></span>
+          <span class="desc-item">the party begins</span>
+          <span class="desc-item"><a href="http://goo.gl/maps/d6YFp">Media Lab<span class="typicn export"></span></a></span>
+          <div class="clearfix"></div>
+        </div>
+        <div class="event">
+          <span class="event-name">TechTalks</span>
+          <span class="time-item">Feb 8 <strong>6-10PM</strong></span>
+          <span class="desc-item">LOCATION TBD</span>
         </div>
       </div>
 
       <div class="tile main-fair">
-        <h2>Techfair 2013</h2>
-        <img class="map" width="250" src="http://maps.googleapis.com/maps/api/staticmap?center=MIT+W33&size=250x250&scale=2&sensor=false&markers=color:blue%7Clabel:T%7CMIT+W33" />
+        <h2 class="techfair2013">Techfair 2013</h2>
         <p>Kick off your spring semester with Techfair 2013. Featuring the
         largest number of companies and student exhibitors ever (really, we
         can't fit more), it's going to be an event you won't want to miss.</p>
-        <dl>
-          <dt>Location</dt>
-          <dd>Rockwell Cage, MIT Building W33</dd>
-          <dt>Time and Date</dt>
-          <dd>February 4th, 2013 - 10am-3pm</dd>
-          <dt>
-        </dl>
+        <div class="event-details">
+          <img class="map" width="200" src="http://maps.googleapis.com/maps/api/staticmap?center=MIT+W33&size=200x200&scale=2&sensor=false&markers=color:blue%7Clabel:T%7CMIT+W33" />
+          <div class="fair-detail">
+            <span class="typicn location"></span>
+            <div class="detail-caption">
+              Rockwell Cage <span class="sub">MIT Building W33</span>
+              <p class="address">
+                120 Vassar St<br />
+                Cambridge, MA 02139
+              </p>
+              <p>
+                (See it on <a href="http://goo.gl/maps/vwbqj">Google Maps <span class="typicn export nofloat"></span></a>)
+              </p>
+            </div>
+          </div>
+          <div class="fair-detail">
+            <span class="typicn time"></span>
+            <div class="detail-caption">
+              Mon, Feb 4 2013, 10am-3pm
+            </div>
+          </div>
+          <div class="fair-detail">
+            <span class="typicn thumbsUp"></span>
+            <div class="detail-caption">
+              Casual attire
+            </div>
+          </div>
+          <div class="fair-detail">
+            <span class="typicn thumbsDown"></span>
+            <div class="detail-caption">
+              No resumes!
+              <p>
+                Drop your resume <a href="http://techfair.mit.edu/drop">online <span class="typicn export nofloat"></span></a> instead.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tile">
+        <h2>Hackathon</h2>
+        <p>Techfair is proud to bring to the MIT community the <a
+        href="/hack">Techfair Hackathon (sponsored by Palantir)</a>.</p>
+        <img height="75" src="/img/homepage/palantir.png" />
+        <p>We've worked hard to get thousands of prize dollars on the line,
+        three awesome meals, notable judges, and competitors from throughout
+        the northeast. Battle it out over a 12 hour period and BRING
+        HONOR AND GLORY TO YOUR SCHOOL.</p>
+        <p>Visit <a href="/hack">http://techfair.mit.edu/hack</a> for more
+        details, and be sure to <a
+        href="http://techfairhack2013.eventbrite.com">register on
+        Eventbrite</a>.
+        <div class="event-details">
+          <div class="fair-detail">
+            <span class="typicn location"></span>
+            <div class="detail-caption">
+              Ray and Maria Stata Center
+              <p class="address">
+                32 Vassar St<br />
+                Cambridge, MA 02139
+              </p>
+              <p>
+                (See it on <a href="http://goo.gl/maps/R3EKa">Google Maps <span class="typicn export nofloat"></span></a>)
+              </p>
+            </div>
+          </div>
+          <div class="fair-detail">
+            <span class="typicn time"></span>
+            <div class="detail-caption">
+              Sat Feb 2 2013, 8pm - Sun Feb 3 2013, 3pm
+            </div>
+          </div>
+        </div>
       </div>
 
       <?php make_footer();?>
