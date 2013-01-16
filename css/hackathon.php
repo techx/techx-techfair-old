d1a73f9115a896e98924e193733f7c08f752e6fe
Ben Frank <benjaminfrank@gmail.com>
2013-01-16 00:30:11 -0500

    Hackathon page updates

---
+++ css/hackathon.php
@@ -0,0 +1,81 @@
+<div class="column-top">
+<?php if($_GET['msg']=='success'):?>
+<div class="success">Your email has been saved. Thank you for your interest in MIT Techfair!</div>
+<?php endif;?>
+  <div class="full">
+    <h1>
+    HackMIT
+    </h1>
+    <h2>
+    Sponsored by&nbsp;<img height="35" width="140" src="/img/palantir_logo.png" style="vertical-align:-12%" alt="palantir"/>&nbsp;
+    </h2>  
+  </div>
+  <div class="full">
+    <a href="http://techfairhack2013.eventbrite.com/">Registration is open!</a>
+  </div>
+</div>
+
+<div class="full">
+  <!--<h2>Interested?</h2>
+  <!--
+  <div id="facebook" class="interest">
+  	<p>Enter your MIT email for more details:</p>
+  	<?php if (isset($error['email'])) echo '<div class="error">',$error['email'],'</div>';?>
+  	<form action="" method="post">
+  		<input type="hidden" name="action" value="hackathon" />
+  		<input type="text" name="email" />
+  		<button type="submit">Submit</button>
+  	</form>
+  </div>
+
+  <p><a href="https://www.facebook.com/events/226472340766270/">Sign up</a> on the official Facebook event page.</p>
+  <p>Please RSVP to this event and add your first and last name and school to the event wall.</p>-->
+
+  <h2>About</h2>
+    <p>Techfair and Palantir are teaming up to bring you MIT’s biggest hackathon of the year. 
+      Represent your school and compete against college students of the northeast. 
+      Prove your skills in either the veteran coder or novice league and tap into our $3,000+ prize pool, including $1,500
+      and direct entry into Greylock Hackfest 2 for the top team! </p>
+  <h2>Schedule</h2>
+    <p>
+
+      <b>Saturday, 2/2 </b><br>
+      8:00pm: Doors open <br>
+      8:30pm: Dinner is served <br>
+      9:00pm: Hackathon Kickoff with [speaker] from Palantir <br>
+      <br>
+      <b>Sunday, 2/3 </b><br>
+      1:00pm: Hacking ends, lunch is served <br>
+      1:30pm: Presentations and awards <br>
+
+    </p>
+
+</div>
+
+<div class="column-right">
+	<h2>Eligibility and Deadlines</h2>
+	  <p> - All university students are invited to compete <br>
+      - Register by <b>January 30th</b> to be guaranteed a spot <br>
+      - Maximum of <b>4</b> students per team
+    </p>
+  <!--<h2>Prizes</h2>
+  <p>Hackathon winners from each school will receive a pair of Beats by Dr. Dre headphones and a trip to Palo Alto to compete with winning teams from other university Hackathons</p>
+	<p>Second prize winners from each school will be rewarded with Diddy Beats</p>
+  <p>There will also be food, drink, and swag for everyone!</p>
+  <h2>Time</h2>
+  <p>Saturday, February 4 @ 8 PM to Sunday, February 5 @ 5 PM</p>
+  <h2>Location</h2>
+  <p>26-152</p>-->
+
+  <h2>Rookie Division</h2>
+    <p>If you don't have much experience coding, enter our <b>rookie</b> division. This is a great chance to expand your programming skills, 
+      meet new people, and win some awesome prizes as well!</p>
+
+</div>
+
+<div class="column-bottom" style="text-align: center; padding-top: 30px;">
+  <a href="/img/hackathon/Hack1o.jpg"><img src="/img/hackathon/Hack1.jpg" style="margin-right: 20px;" width=220 height=150></a>
+  <a href="/img/hackathon/Hack2o.jpg"><img src="/img/hackathon/Hack2.jpg" style="margin-right: 20px;" width=220 height=150></a>
+  <a href="/img/hackathon/Hack3o.jpg"><img src="/img/hackathon/Hack3.jpg" style="margin-right: 20px;" width=220 height=150></a>
+  <a href="/img/hackathon/Hack4o.jpg"><img src="/img/hackathon/Hack4.jpg" width=220 height=150></a>
+</div>
---
+++ parts/header_save.php
@@ -0,0 +1,108 @@
+<?php
+function make_header($sectionName,$subSectionName,$routes)
+{
+?>
+    <div id="header">
+	    	<a id="logo" href="/">Techfair</a>
+	</div>
+		<ul id="nav">
+			<?php
+			$k = 0;
+			foreach($routes as $key=>$section):?>
+				<?php
+				if($key!='home' && !isset($section['show'])):
+					$printkey = $key.'/';
+				?>
+					<?php ($sectionName==$key) ? $class=' current' : $class='';?>
+					<?php ($key=='think') ? $think=' think' : $think='';?>
+					<?php if($key!='think'): /*Don't generate the THINK link for the navigation*/?>
+					
+					<li id="menu<?php echo $k?>">
+						<?php if(!isset($section['external'])):?>
+							<a href="/<?php echo $printkey?>"><?php echo $section['name']?></a>
+						<?php else:?>
+							<a href="<?php echo $section['external']?>"><span><?php echo $section['name']?></span></a>
+						<?php endif;?>
+					</li>
+					<? endif;?>
+					
+				<?php
+				$k++;
+				endif;?>
+			<?php endforeach;?>
+					<li id="submenu">
+						<?php
+						$k = 0;
+						foreach($routes as $key=>$section):?>
+							<?php
+							if($key!='home' && !isset($section['show'])):
+								$printkey = $key.'/';
+							?>
+								<?php ($sectionName==$key) ? $class=' current' : $class='';?>
+								<?php ($key=='think') ? $think=' think' : $think='';?>
+									
+									<?php if(isset($section[''])): //only folders have a key named '' for the main page?>
+										<?php ($sectionName == $key) ? $class = 'shown' : $class = 'hidden'; ?>
+										<ul id="sub<?php echo $k?>">
+											
+											<?php $uniqueName=''; 
+										 		switch($printkey){
+													case "events/":
+														$uniqueName = "Summary of 2012 Events";
+														break;
+													case "companies/":
+														$uniqueName = "Get Involved";
+														break;
+													case "students/":
+														$uniqueName = "Get Involved";
+														break;
+													case "about/":
+														$uniqueName = "About Techfair";
+														break;
+												}
+											?>
+											<li><a href="/<?php echo $printkey?>"><?php echo $uniqueName?></a></li>
+											
+											
+											<?php foreach($section as $subkey=>$page): ?>
+												<?php if(is_array($page) && $subkey != ''):?>
+													
+													<!-- BIG LIST OF EXCEPTIONS -->
+													<?php if($page['name']=="Demos"):
+														continue;
+														endif;
+													?>
+													<?php if($page['name']=="The Fair"):
+															$fairClass = "bold";
+															$page['name'] = '<img src="/img/right.png"> The Fair';
+														  elseif($page['name']=="2012 Photos"):
+															$fairClass = "bold";
+															$page['name'] = '<img src="/img/right.png"> 2012 Photos';
+														  else:
+															$fairClass = "fillerClass";
+														  endif;	?>
+														
+														<?php if($page['name']=="2012 Resume Drop"):
+																$page['name'] = '<img src="/img/right.png"> 2012 Resume Drop <img src="/img/external-grey.png">';
+															  endif;	?>
+															
+													<!-- END LIST OF EXCEPTIONS -->	
+													
+													<?php if(isset($page['content'])):?>
+														<?php ($sectionName==$key && $subSectionName==$subkey) ? $class=' class="current"' : $class='';?>
+														<li<?php echo $class?>><a class=<?php echo $fairClass ?> href="/<?php echo $key?>/<?php echo $subkey ?>/"><?php echo $page['name']?></a></li>
+													<?php else:?>
+														<li><a class=<?php echo $fairClass ?> href="<?php echo $page['external']?>"><?php echo $page['name']?></a></li>
+													<?php endif;?>
+									
+												<?php endif;?>
+											<?php endforeach;?>
+										</ul>
+									<?php endif;?>
+							<?php
+							$k++;
+							endif;?>
+						<?php endforeach;?>
+					</li>
+		</ul>
+<?php } ?>
