b41f47d8807763bde9df1a3ba2a5effa7ae5307d
Ankush Gupta <ankush@mit.edu>
2013-09-26 11:58:21 -0400

    updated application forms to use techfair wufoo

--- pages/applications/platinum.php
+++ pages/applications/platinum.php
@@ -1,1 +1,18 @@
-<iframe height="845" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="http://ankushg.wufoo.com/embed/q7x2x3/"><a href="http://ankushg.wufoo.com/forms/q7x2x3/">Fill out my Wufoo form!</a></iframe>
+<div id="wufoo-z7p9m3">
+Fill out our <a href="https://techfair.wufoo.com/forms/z7p9m3">online application for Platinum sponsorship</a>.
+</div>
+<script type="text/javascript">var z7p9m3;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'techfair', 
+'formHash':'z7p9m3', 
+'autoResize':true,
+'height':'808',
+'async':true,
+'header':'show', 
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { z7p9m3 = new WufooForm();z7p9m3.initialize(options);z7p9m3.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
--- pages/applications/sponsor.php
+++ pages/applications/sponsor.php
@@ -1,1 +1,18 @@
-<iframe height="815" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="http://ankushg.wufoo.com/embed/m7x3q1/"><a href="http://ankushg.wufoo.com/forms/m7x3q1/">Fill out my Wufoo form!</a></iframe>
+<div id="wufoo-z7x0q1">
+Fill out our <a href="https://techfair.wufoo.com/forms/z7x0q1">online application for Platinum sponsorship</a>.
+</div>
+<script type="text/javascript">var z7x0q1;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'techfair', 
+'formHash':'z7x0q1', 
+'autoResize':true,
+'height':'905',
+'async':true,
+'header':'show', 
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { z7x0q1 = new WufooForm();z7x0q1.initialize(options);z7x0q1.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
